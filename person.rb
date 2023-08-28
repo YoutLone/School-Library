b2b8da36830090e9b0f07c3962aeda0b4350349c
Yout <youtlone95@gmail.com>
2023-08-28 17:18:42 +0630

    add trimmer decorator class

--- person.rb
+++ person.rb
@@ -1,15 +1,18 @@
-class Person
+require_relative 'nameable'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = rand(1..500)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
+    @id = Random.rand(1..500)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
-
-  private
 
   def of_age?
     @age >= 18
@@ -18,4 +21,17 @@ class Person
   def can_use_services?
     of_age? || @parent_permission
   end
+
+  def correct_name
+    @name
+  end
 end
+
+person = Person.new(22, 'maximillianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
