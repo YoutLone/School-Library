5e99173f89be2335b6f5214353fd25c0affdb5a1
Yout <youtlone95@gmail.com>
2023-08-28 11:28:19 +0630

    add README.md file and fix linter error

--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,14 @@
 require_relative 'person'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(age, parent_permission: true, name:'Unknown',specialization: nil)
-        super(age, parent_permission: parent_permission, name: name)
-        @specialization = specialization
-    end
+  def initialize(age, parent_permission: true, name: 'Unknown', specialization: nil)
+    super(age, parent_permission: parent_permission, name: name)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
