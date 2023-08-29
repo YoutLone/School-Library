cf71ab3c7c599bd6124f0dec7cf482207cad3480
Yout <youtlone95@gmail.com>
2023-08-29 20:08:59 +0630

    add Main class ruby file and update another file

--- person.rb
+++ person.rb
@@ -1,6 +1,4 @@
 require_relative 'nameable'
-require_relative 'capitalize_decorator'
-require_relative 'trimmer_decorator'
 
 class Person < Nameable
   attr_reader :id, :rentals
@@ -33,12 +31,3 @@ class Person < Nameable
     Rental.new(book, date, self)
   end
 end
-
-# person = Person.new(22, 'maximillianus')
-# puts person.correct_name
-
-# capitalized_person = CapitalizeDecorator.new(person)
-# puts capitalized_person.correct_name
-
-# capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-# puts capitalized_trimmed_person.correct_name
