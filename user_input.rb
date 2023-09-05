698f56cc8b09e68820dc398b1cf57f1365ce9f34
Yout <youtlone95@gmail.com>
2023-09-05 23:14:30 +0630

    update of user input file

--- user_input.rb
+++ user_input.rb
@@ -1,22 +1,19 @@
-# rubocop:disable Metrics/CyclomaticComplexity
 def input(num, app)
-  case num
-  when 1
-    app.list_books
-  when 2
-    app.list_people
-  when 3
-    app.create_person
-  when 4
-    app.create_book
-  when 5
-    app.create_rental
-  when 6
-    app.list_rentals
-  when 7
-    puts 'Thank you for using this app.See you soon again!'
+  actions = {
+    1 => -> { app.list_books },
+    2 => -> { app.list_people },
+    3 => -> { app.create_person },
+    4 => -> { app.create_book },
+    5 => -> { app.create_rental },
+    6 => -> { app.list_rentals },
+    7 => -> { puts 'Thank you for using this app. See you soon again!' }
+  }
+
+  action = actions[num]
+
+  if action
+    action.call
   else
-    puts "No acction matches your selection(#{num}), Kindly provide a valid input"
+    puts "No action matches your selection (#{num}). Kindly provide a valid input."
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
 end
