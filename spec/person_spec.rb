9f2b51f401eb7801f9b15f9ac224f56090de69bf
Zafron047 <117600135+Zafron047@users.noreply.github.com>
2023-09-06 18:08:39 +0600

    Add a new context if constructor method sets the default value for:
    optional name parameter if it is not provided.

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,8 +1,11 @@
 require_relative '../person'
+require 'date'
 
 describe Person do
   let(:person) { Person.new(25, 'John Doe') }
   let(:young_person) { Person.new(15, 'Alice', parent_permission: true) }
+  let(:book) { double('book') }
+  let(:date) { Date.new(2023, 9, 6) }
 
   context 'attributes' do
     it 'has an ID' do
@@ -35,6 +38,14 @@ describe Person do
     it 'is not of age and does not have parent permission' do
       young_person.instance_variable_set(:@parent_permission, false)
       expect(young_person.send(:can_use_services?)).to be false
+    end
+  end
+
+  # The constructor method should set the default value for the optional name parameter if it is not provided.
+  context 'constructor' do
+    it 'sets the default value for the optional name parameter if not provided' do
+      unnamed_person = Person.new(30)
+      expect(unnamed_person.name).to eq('Unknown')
     end
   end
 end
